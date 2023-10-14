a66d12707406902aefe049393f44633a62850f75
Arthur-Vic <arthurvicto99@gmail.com>
2023-10-14 20:04:30 -0300

    improved ux

--- functionality.js
+++ functionality.js
@@ -6,19 +6,44 @@ const list = $(".list");
 //   console.log("click");
 // }
 
-$("#add-item").on("click", function () {
+function addItem() {
   let inputValue = $("#item-input").val();
   if (inputValue !== "") {
     let newLi = $("<li>");
-    newLi.text(inputValue);
+    let newSpan = $("<span>").text(inputValue);
+    newSpan.addClass("text");
+    newLi.append(newSpan);
     newLi.addClass("item");
     $(".scrollable").append(newLi);
-    let newSpan = $("<span>");
-    newSpan.html("\u00d7");
-    newLi.append(newSpan);
+    let newImg = $("<img>");
+    newImg.attr("src", "icons/icons8-trash-can.svg");
+    newImg.attr("width", "30px");
+    newImg.addClass("trash");
+    newLi.append(newImg);
+    $("#item-input").val("");
+  }
+}
+
+$("#item-input").on("keypress", function (e) {
+  if (e.keyCode === 13) {
+    addItem();
   }
 });
 
-$(".scrollable").on("click", ".item", function () {
-  $(this).addClass("check");
+$("#add-item").on("click", function () {
+  addItem();
+});
+
+$(".scrollable").on("click", "li .text", function () {
+  $(this).toggleClass("check");
+});
+
+$(".scrollable").on("click", "li .trash", function () {
+  $(this).parent().remove();
+});
+
+$(".clear").on("click", function () {
+  if ($(".item").length > 0) {
+    $(".item").remove();
+  }
 });
