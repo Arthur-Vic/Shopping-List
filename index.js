8b9af4b45603f9f0423d0830a842e0f3f35d57ac
Arthur-Vic <arthurvicto99@gmail.com>
2023-10-23 14:45:44 -0300

    changes for server functionality using get and post requests

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-// import express from "express";
+// import bodyParser from "body-parser";
+import bodyParser from "body-parser";
+import express from "express";
 
-// const app = express();
+const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.render("index.ejs");
-// });
+// app.set("views", "./views");
+app.set("view engine", "ejs");
 
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}.`);
-// });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//List Management ---------------------------
+let items = [
+  { text: "Item 1", checked: false },
+  { text: "Item 2", checked: false },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { items });
+});
+
+app.post("/add", (req, res) => {
+  let newText = req.body.newItem;
+  let newItem = { text: newText, checked: false };
+  items.push(newItem);
+  res.redirect("/");
+});
+
+app.post("/remove", (req, res) => {
+  let itemIndex = req.body.index;
+  items.splice(itemIndex, 1);
+  res.redirect("/");
+});
+
+app.post("/toggleCheck", (req, res) => {
+  let itemIndex = req.body.index;
+  items[itemIndex].checked = !items[itemIndex].checked;
+  res.redirect("/");
+  console.log(items);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
